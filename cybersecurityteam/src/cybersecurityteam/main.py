7e1500b32f5963e5a4402e7aeaf851783d16f77f
ThiagoPGPEsteves <147446495+ThiagoPGPEsteves@users.noreply.github.com>
2025-03-27 06:33:52 -0300

    .

--- cybersecurityteam/src/cybersecurityteam/main.py
+++ cybersecurityteam/src/cybersecurityteam/main.py
@@ -4,7 +4,7 @@ import warnings
 
 from datetime import datetime
 
-from crew import Cybersecurityteam
+from crew import cybersecurityteam
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
@@ -17,8 +17,8 @@ def run():
     """
     Run the crew.
     """
+    print( "5")
     inputs = {
-        'topic': 'AI LLMs',
         'current_year': str(datetime.now().year)
     }
     
@@ -26,41 +26,6 @@ def run():
         Cybersecurityteam().crew().kickoff(inputs=inputs)
     except Exception as e:
         raise Exception(f"An error occurred while running the crew: {e}")
+run()
 
 
-def train():
-    """
-    Train the crew for a given number of iterations.
-    """
-    inputs = {
-        "topic": "AI LLMs"
-    }
-    try:
-        Cybersecurityteam().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while training the crew: {e}")
-
-def replay():
-    """
-    Replay the crew execution from a specific task.
-    """
-    try:
-        Cybersecurityteam().crew().replay(task_id=sys.argv[1])
-
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
-
-def test():
-    """
-    Test the crew execution and returns the results.
-    """
-    inputs = {
-        "topic": "AI LLMs",
-        "current_year": str(datetime.now().year)
-    }
-    try:
-        Cybersecurityteam().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while testing the crew: {e}")
